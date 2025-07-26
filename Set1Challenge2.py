ce7ff1f131cba4ac22ca680e3624dcd9559ce629
Jordan Farrow <jordomine01@outlook.com>
2025-07-26 10:24:08 +0800

    Completed from set 1:
    Challenge 1
    Challenge 2

--- Set1Challenge2.py
+++ Set1Challenge2.py
@@ -1,6 +1,21 @@
-
-#write a function to XOR a hex string with 
+#write a function to XOR two hex strings together
 
 def XOR(hex1string, hex2string):
+    result = bytearray()
+    #convert both hex strings into bytes
+    hex1Bytes = bytes.fromhex(hex1string)
+    hex2Bytes = bytes.fromhex(hex2string)
+    #pairs bytes together for XOR
+    test = zip(hex1Bytes, hex2Bytes)
+
+    for byte1, byte2 in test:
+        #XORs pairs
+        result.append(byte1 ^ byte2)
+
+    return result.hex()
+        
+
+XOR('1c0111001f010100061a024b53535009181c', '686974207468652062756c6c277320657965')
+
 
 
